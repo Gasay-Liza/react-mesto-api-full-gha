8218312b83a4f5b947e3ec12f0dfda95f367e978
Elizaveta Gasay <gasayliz@yandex.ru>
2023-05-22 20:36:07 +0300

    bagfix

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 
 app.use(cors);
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3002 } = process.env;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
